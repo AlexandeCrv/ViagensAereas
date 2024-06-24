df5ba52949e2a2732763d3ca2249626dac745d12
AlexandeCrv <alexandecarvalho318@gmail.com>
2024-06-24 20:32:44 -0300

    tielinha

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: "/",
+        path: "/ViagensAereas",
         element: <Home />,
       },
       {
